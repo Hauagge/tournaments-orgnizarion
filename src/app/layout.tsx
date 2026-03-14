50ffd59bfb7e7fa25552d3f2a2aa8b4a969cc091
Hauagge <gsantos@viptech.com.br>
2026-03-14 14:31:08 -0300

    feat: integrate AppQueryProvider for React Query management in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { AppQueryProvider } from '@/shared/providers/query-provider';
 import { Toaster, ToastProvider } from '@/shared/ui/use-toast';
 
 const geistSans = Geist({
@@ -28,10 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ToastProvider>
-          {children}
-          <Toaster position="top-right" />
-        </ToastProvider>
+        <AppQueryProvider>
+          <ToastProvider>
+            {children}
+            <Toaster position="top-right" />
+          </ToastProvider>
+        </AppQueryProvider>
       </body>
     </html>
   );
