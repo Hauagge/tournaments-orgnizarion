d88729233d6235ec089b53c2191d880414729412
Hauagge <gsantos@viptech.com.br>
2026-04-02 14:22:23 -0300

    feat: add AreasDistributionRoute component to render AreasDistributionPage

---
+++ src/app/(admin)/areas/distribution/page.tsx
@@ -0,0 +1,5 @@
+import AreasDistributionPage from '@/features/areas/components/areas-distribution-page';
+
+export default function AreasDistributionRoute() {
+  return <AreasDistributionPage />;
+}
